aacf8b4f888e10b2a5ceb7cf9743f0562ec7ff73
f3kilo <f3kilo@yandex.ru>
2022-04-12 11:11:50 +0300

    Ship.

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,17 @@ async fn main() {
 
 struct Game {
     start_time: f64,
+    last_update: f64,
+    ship: Ship,
 }
 
 impl Default for Game {
     fn default() -> Self {
+        let time = get_time();
         Self {
-            start_time: get_time(),
+            start_time: time,
+            last_update: time,
+            ship: Ship::default(),
         }
     }
 }
@@ -32,6 +37,11 @@ impl Game {
             return Some(get_time() - self.start_time);
         }
 
+        let elapsed_time = self.elapsed_time();
+
+        self.ship.update(elapsed_time);
+
+        self.last_update = get_time();
         None
     }
 
@@ -39,8 +49,13 @@ impl Game {
         get_time() - self.start_time
     }
 
+    fn elapsed_time(&self) -> f64 {
+        get_time() - self.last_update
+    }
+
     pub fn draw(&self, best_time: f64) {
         self.draw_time(best_time);
+        self.ship.draw();
     }
 
     fn draw_time(&self, best_time: f64) {
@@ -96,18 +111,76 @@ impl State {
         if let Some(game) = &self.game {
             game.draw(self.best_time)
         } else {
-            draw_menu()
+            Self::draw_menu()
+        }
+    }
+
+    pub fn draw_menu() {
+        let font_size = 40.0;
+        let text = "Press Enter to start game.";
+        let text_size = measure_text(text, None, font_size as _, 1.0);
+        let text_pos = (
+            (screen_width() - text_size.width) / 2.0,
+            (screen_height() - text_size.height) / 2.0,
+        );
+        draw_text(text, text_pos.0, text_pos.1, font_size, BLACK);
+    }
+}
+
+pub struct Ship {
+    position: f32,
+    speed: f32,
+}
+
+impl Default for Ship {
+    fn default() -> Self {
+        Self {
+            position: screen_width() / 2.0,
+            speed: 0.0,
         }
     }
 }
 
-pub fn draw_menu() {
-    let font_size = 40.0;
-    let text = "Press Enter to start game.";
-    let text_size = measure_text(text, None, font_size as _, 1.0);
-    let text_pos = (
-        (screen_width() - text_size.width) / 2.0,
-        (screen_height() - text_size.height) / 2.0,
-    );
-    draw_text(text, text_pos.0, text_pos.1, font_size, BLACK);
+impl Ship {
+    const SHIP_WIDTH: f32 = 25.0;
+    const SHIP_HEIGHT: f32 = 50.0;
+    const SHIP_OFFSET: f32 = 30.0;
+
+    pub fn update(&mut self, elapsed_time: f64) {
+        const ACCELERATION: f32 = 200.0;
+        const DECELERATION: f32 = 180.0;
+        let elapsed_time = elapsed_time as f32;
+
+        self.speed /= DECELERATION * elapsed_time;
+
+        if is_key_down(KeyCode::A) {
+            self.speed -= ACCELERATION * elapsed_time;
+        }
+
+        if is_key_down(KeyCode::D) {
+            self.speed += ACCELERATION * elapsed_time;
+        }
+
+        self.position += self.speed;
+        self.position = self.position.clamp(
+            Self::SHIP_WIDTH / 2.0,
+            screen_width() - Self::SHIP_WIDTH / 2.0,
+        );
+    }
+
+    pub fn draw(&self) {
+        let top = Vec2::new(
+            self.position,
+            screen_height() - Self::SHIP_HEIGHT / 2.0 - Self::SHIP_OFFSET,
+        );
+        let left = Vec2::new(
+            self.position - Self::SHIP_WIDTH / 2.0,
+            screen_height() - Self::SHIP_OFFSET,
+        );
+        let right = Vec2::new(
+            self.position + Self::SHIP_WIDTH / 2.0,
+            screen_height() - Self::SHIP_OFFSET,
+        );
+        draw_triangle(top, right, left, WHITE)
+    }
 }
